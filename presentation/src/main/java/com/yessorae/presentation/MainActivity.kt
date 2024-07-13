fc353e1c77a5c11a67caf04246304bd332f3f14c
yessorae <yessorae@gmail.com>
2024-07-13 23:40:55 +0900

    chore: 패키지 정리

--- presentation/src/main/java/com/yessorae/presentation/MainActivity.kt
+++ presentation/src/main/java/com/yessorae/presentation/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.yessorae.presentation.ui.theme.LezhinAssignmentTheme
+import com.yessorae.presentation.theme.LezhinAssignmentTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
--- presentation/src/main/java/com/yessorae/presentation/theme/Type.kt
+++ presentation/src/main/java/com/yessorae/presentation/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.yessorae.presentation.ui.theme
+package com.yessorae.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
