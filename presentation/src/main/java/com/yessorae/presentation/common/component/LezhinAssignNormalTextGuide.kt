8bfb03e0f9203b93575b3c31549e090cdae7a4c5
yessorae <yessorae@gmail.com>
2024-07-14 23:40:01 +0900

    feat: 검색화면 기능추가 - 사용자는 검색결과가 없을때 이를 알 수 있다., 사용자는 검색결과에 오류가 발생했을 때 이를 알 수 있다.

--- presentation/src/main/java/com/yessorae/presentation/common/component/LezhinAssignNormalTextGuide.kt
+++ presentation/src/main/java/com/yessorae/presentation/common/component/LezhinAssignNormalTextGuide.kt
@@ -1,24 +1,23 @@
 package com.yessorae.presentation.common.component
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import com.yessorae.presentation.R
 
 @Composable
-fun LezhinAssignmentErrorGuide(modifier: Modifier = Modifier) {
+fun LezhinAssignmentNormalTextGuide(
+    modifier: Modifier = Modifier,
+    title: String
+) {
     Box(
         modifier = modifier,
         contentAlignment = Alignment.Center
     ) {
         Text(
-            text = stringResource(id = R.string.common_error_message),
-            color = MaterialTheme.colorScheme.error,
+            text = title,
             textAlign = TextAlign.Center
         )
     }
