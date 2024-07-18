778f2a862714164d7c87676ca37f2bb46b4fc354
yessorae <yessorae@gmail.com>
2024-07-18 23:50:12 +0900

    #15 키보드에 done 버튼 제거

--- presentation/src/main/java/com/yessorae/presentation/common/component/LezhinAssignmentTextField.kt
+++ presentation/src/main/java/com/yessorae/presentation/common/component/LezhinAssignmentTextField.kt
@@ -1,5 +1,6 @@
 package com.yessorae.presentation.common.component
 
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
@@ -9,6 +10,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import com.yessorae.presentation.R
 
 @Composable
@@ -25,6 +27,7 @@ fun LezhinAssignmentTextField(
         modifier = modifier,
         singleLine = true,
         placeholder = { Text(text = hint) },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.None),
         trailingIcon = {
             if (keyword.isNotBlank()) {
                 IconButton(onClick = onClickClearKeyword) {
