eebdbf29f810509c9687a87a08908ef6a4fd05c4
yessorae <yessorae@gmail.com>
2024-07-19 01:26:50 +0900

    #15 북마크 멀티 제거할 때 이미지 선택한 것이 명확하게 아이콘 추가

--- presentation/src/main/java/com/yessorae/presentation/screen/bookmark/component/SelectableBookmarkListItem.kt
+++ presentation/src/main/java/com/yessorae/presentation/screen/bookmark/component/SelectableBookmarkListItem.kt
@@ -5,10 +5,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.yessorae.presentation.R
 import com.yessorae.presentation.common.component.LezhinAssignmentImage
 import com.yessorae.presentation.screen.bookmark.model.SelectableBookmarkImageUi
@@ -27,13 +33,22 @@ fun SelectableBookmarkListItem(
             .fillMaxWidth()
             .aspectRatio(aspectRatio)
             .clickable(onClick = onClick)
-            .alpha(if (selectableBookmarkImageUi.selected) 0.5f else 1f)
+            .alpha(if (selectableBookmarkImageUi.selected) 0.5f else 1f),
+        contentAlignment = Alignment.Center
     ) {
         LezhinAssignmentImage(
             imageUrl = bookmarkImageUi.imageUrl,
             thumbnailUrl = bookmarkImageUi.thumbnailUrl,
             modifier = Modifier.fillMaxSize(),
-            contentDescription = stringResource(id = R.string.content_description_bookmark_search_result)
+            contentDescription = stringResource(id = R.string.content_description_bookmark_multi_select)
         )
+
+        if (selectableBookmarkImageUi.selected) {
+            Icon(
+                imageVector = Icons.Default.Check,
+                contentDescription = stringResource(id = R.string.content_description_bookmark_multi_select),
+                modifier = Modifier.size(48.dp)
+            )
+        }
     }
 }
