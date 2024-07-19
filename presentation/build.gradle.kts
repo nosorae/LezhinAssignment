50d55ee523812d7c5645d086d2f4d6cdffb40c36
yessorae <yessorae@gmail.com>
2024-07-20 00:42:13 +0900

    #20 사용되지 않는 라이브러리 의존성/코드 제거

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -53,11 +53,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.windowSizeClass)
-    debugImplementation(libs.androidx.ui.tooling)
 
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.comopse)
 
